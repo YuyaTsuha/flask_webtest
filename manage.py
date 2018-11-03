ea6163f15927eb4d8a7b68c188a6bcb934abc52a
YuyaTsuha <yato0719@yahoo.co.jp>
2018-11-03 14:08:18 +0900

    maigrate
    マイグレートできるようにしました

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from flask import Flask, render_template
-from models import db
+from models import *
 from flask.ext.script import Manager, Server
 from flask.ext.migrate import Migrate, MigrateCommand
 
@@ -11,7 +11,7 @@ app.config['DEBUG'] = True
 #秘密キー
 app.secret_key = 'development key'
 #データベースを指定
-app.config['SQLALCHEMY_DATABASE_URI'] = 'SQLite:///diary.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
 app.config['SQLALCHEMY_NATIVE_UNICODE'] = 'utf-8'
 db.init_app(app)
 db.app = app
@@ -19,6 +19,15 @@ db.app = app
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+@app.route("/book/index")
+def book_index():
+    """
+    書籍一覧
+    """
+    a = Book.query.order_by(Book.id.asc())
+    return render_template("book/index.html",
+                            books=a)
 
 migrate = Migrate(app, db)
 manager = Manager(app)
